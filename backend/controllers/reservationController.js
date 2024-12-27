f3f77a95cb35e0c48e642e18d561b0a7ef2cd2f2
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2024-12-28 01:41:16 +0200

    Update reservationController.js

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -7,9 +7,9 @@ const reviews_vezeeta = require('../utils/reviews_vezeeta.json')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const streamifier = require('streamifier');
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 
 
 //Create a Place
@@ -33,34 +33,34 @@ exports.createReservation = async (req, res, next) => {
         let placeObj = await Place.findById(newObj.reservationDetails.place)
         reservation = await Reservation.findById(reservation._id).populate('user').populate('reservationDetails.place')
         console.log(placeObj)
-        client.messages
-            .create({
-                body: `تم تأكيد الحجز بأسم :
-                ${name}
-                يوم : ${newObj.reservationDetails.dayText}, ${newObj.reservationDetails.dayNumber}/${newObj.reservationDetails.month}/${newObj.reservationDetails.year}
-                من ${newObj.reservationDetails.fromTime} إلي ${newObj.reservationDetails.toTime}
-                في : ${placeObj.name} , ${placeObj.address}
-                `,
-                from: '+19723626780',
-                to: reservation.user.phoneNo
+        // client.messages
+        //     .create({
+        //         body: `تم تأكيد الحجز بأسم :
+        //         ${name}
+        //         يوم : ${newObj.reservationDetails.dayText}, ${newObj.reservationDetails.dayNumber}/${newObj.reservationDetails.month}/${newObj.reservationDetails.year}
+        //         من ${newObj.reservationDetails.fromTime} إلي ${newObj.reservationDetails.toTime}
+        //         في : ${placeObj.name} , ${placeObj.address}
+        //         `,
+        //         from: '+19723626780',
+        //         to: reservation.user.phoneNo
 
-            })
-            .then(async message => {
-                console.log(message)
-                await client.messages.create({
-                    body: `تم تأكيد الحجز بأسم :
-                ${name}
-                يوم : ${newObj.reservationDetails.dayText}, ${newObj.reservationDetails.dayNumber}/${newObj.reservationDetails.month}/${newObj.reservationDetails.year}
-                من ${newObj.reservationDetails.fromTime} إلي ${newObj.reservationDetails.toTime}
-                في : ${placeObj.name} , ${placeObj.address}
-                `,
-                    from: '+19723626780',
-                    to: '+201002229745'
+        //     })
+        //     .then(async message => {
+        //         console.log(message)
+        //         await client.messages.create({
+        //             body: `تم تأكيد الحجز بأسم :
+        //         ${name}
+        //         يوم : ${newObj.reservationDetails.dayText}, ${newObj.reservationDetails.dayNumber}/${newObj.reservationDetails.month}/${newObj.reservationDetails.year}
+        //         من ${newObj.reservationDetails.fromTime} إلي ${newObj.reservationDetails.toTime}
+        //         في : ${placeObj.name} , ${placeObj.address}
+        //         `,
+        //             from: '+19723626780',
+        //             to: '+201002229745'
 
-                }).then(async message => {
-                    console.log(message)
-                })
-            });
+        //         }).then(async message => {
+        //             console.log(message)
+        //         })
+        //     });
         reservation = await Reservation.findById(reservation._id).populate('user').populate('reservationDetails.place')
         res.status(200).json({
             success: true,
@@ -105,38 +105,38 @@ exports.cancelReservation = async (req, res, next) => {
     } else {
         reservation.status = "ملغي"
         await reservation.save();
-        client.messages
-            .create({
-                body: `تم الغاء الحجز بأسم :
-            ${reservation.name}
-            يوم : ${reservation.reservationDetails.dayText}, ${reservation.reservationDetails.dayNumber}/${reservation.reservationDetails.month}/${reservation.reservationDetails.year}
-            من ${reservation.reservationDetails.fromTime} إلي ${reservation.reservationDetails.toTime}
-            في : ${reservation.reservationDetails.place.name} , ${reservation.reservationDetails.place.address}
-            `,
-                from: '+19723626780',
-                to: reservation.user.phoneNo
+        // client.messages
+        //     .create({
+        //         body: `تم الغاء الحجز بأسم :
+        //     ${reservation.name}
+        //     يوم : ${reservation.reservationDetails.dayText}, ${reservation.reservationDetails.dayNumber}/${reservation.reservationDetails.month}/${reservation.reservationDetails.year}
+        //     من ${reservation.reservationDetails.fromTime} إلي ${reservation.reservationDetails.toTime}
+        //     في : ${reservation.reservationDetails.place.name} , ${reservation.reservationDetails.place.address}
+        //     `,
+        //         from: '+19723626780',
+        //         to: reservation.user.phoneNo
 
-            })
-            .then(async message => {
-                console.log(message.sid)
-                client.messages
-            .create({
-                body: `تم الغاء الحجز بأسم :
-            ${reservation.name}
-            يوم : ${reservation.reservationDetails.dayText}, ${reservation.reservationDetails.dayNumber}/${reservation.reservationDetails.month}/${reservation.reservationDetails.year}
-            من ${reservation.reservationDetails.fromTime} إلي ${reservation.reservationDetails.toTime}
-            في : ${reservation.reservationDetails.place.name} , ${reservation.reservationDetails.place.address}
-            `,
-                from: '+19723626780',
-                to: '+201002229745'
+        //     })
+        //     .then(async message => {
+        //         console.log(message.sid)
+        //         client.messages
+        //     .create({
+        //         body: `تم الغاء الحجز بأسم :
+        //     ${reservation.name}
+        //     يوم : ${reservation.reservationDetails.dayText}, ${reservation.reservationDetails.dayNumber}/${reservation.reservationDetails.month}/${reservation.reservationDetails.year}
+        //     من ${reservation.reservationDetails.fromTime} إلي ${reservation.reservationDetails.toTime}
+        //     في : ${reservation.reservationDetails.place.name} , ${reservation.reservationDetails.place.address}
+        //     `,
+        //         from: '+19723626780',
+        //         to: '+201002229745'
 
-            })
-            .then(message => {
-                console.log(message.sid)
+        //     })
+        //     .then(message => {
+        //         console.log(message.sid)
             
-            });
+        //     });
             
-            });
+        //     });
         reservation = await Reservation.findById(req.params.id).populate('user').populate('reservationDetails.place')
         res.status(200).json({
             success: true,

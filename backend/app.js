928efe1c8240d3d41610c43f757f05fe99e4d342
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2024-12-28 03:02:42 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use("/api/v1", reservation);
 if (process.env.NODE_ENV === "PRODUCTION") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get('*', function (req, res) {
-  const index = path.join(__dirname, '../frontend/build', 'index.html');
+  const index = path.join('../frontend/build', 'index.html');
   res.sendFile(index);
 });
 }
